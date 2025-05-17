71d7f48de2586ea3326dfa381398b7720c352f01
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2025-05-18 06:18:23 +0700

    feat: make barcode unique in products table.

--- app/Filament/Tenant/Resources/ProductResource/Traits/HasProductForm.php
+++ app/Filament/Tenant/Resources/ProductResource/Traits/HasProductForm.php
@@ -151,6 +151,7 @@ trait HasProductForm
         return TextInput::make('barcode')
             ->helperText(__('Point the cursor to this input first then scan the barcode'))
             ->visible(Feature::active(ProductBarcode::class))
+            ->unique()
             ->translateLabel();
     }
 
